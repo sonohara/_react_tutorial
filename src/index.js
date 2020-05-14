39ddb4afeb9361da9662fb7bb842c19477d52cbd
sonohara <5350767+sonohara@users.noreply.github.com>
2020-05-14 20:03:49 +0900

    ゲームを完成させる > 手番の処理

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
@@ -46,12 +47,12 @@ class Board extends React.Component {
     // 配列のコピーを作成する
     // https://ja.reactjs.org/tutorial/tutorial.html#why-immutability-is-important
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
